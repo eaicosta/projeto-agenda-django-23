d9d32778e0851ac1b5e1c996a2a624ed792086e8
Davi Costa <davicostadelira04@gmail.com>
2023-06-19 22:47:00 -0300

    Criando o template, a view e a url para exibir contato único

--- contact/views/contact_views.py
+++ contact/views/contact_views.py
@@ -1,4 +1,4 @@
-from django.shortcuts import render
+from django.shortcuts import get_object_or_404, render
 
 from contact.models import Contact
 
@@ -8,7 +8,7 @@ from contact.models import Contact
 def index(request):
     contacts = Contact.objects \
         .filter(show=True)\
-        .order_by('-id')[:10]
+        .order_by('-id')[10:20]
 
     context = {
         'contacts': contacts,
@@ -17,5 +17,22 @@ def index(request):
     return render(
         request,
         'contact/index.html',
+        context
+    )
+
+
+def contact(request, contact_id):
+    # single_contact = Contact.objects.filter(pk=contact_id).first()
+    single_contact = get_object_or_404(
+        Contact, pk=contact_id, show=True
+    )
+
+    context = {
+        'contact': single_contact,
+    }
+
+    return render(
+        request,
+        'contact/contact.html',
         context
     )
